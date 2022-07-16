4c8b04d3b31f57d06ecc5d8c90aa53a610011c81
abhishek3008 <abhisheksontakke92@gmail.com>
2022-07-17 01:43:49 +0530

    Tooltip ticks and minor function updates

--- charts/modules/tooltip.js
+++ charts/modules/tooltip.js
@@ -21,8 +21,16 @@ const getToolTopValues = (element) => {
     let { textColumn1: xLabelName, textColumn2: yLabelName, textColumn3: colorByName } = toolTip;
 
     switch (window.grafieks.plotConfiguration.chartName) {
+        case CONSTANTS.DONUT_CHART:
+        case CONSTANTS.PIE_CHART:
         case CONSTANTS.HORIZONTAL_BAR_CHART:
         case CONSTANTS.BAR_CHART:
+        case CONSTANTS.LINE_CHART:
+        case CONSTANTS.AREA_CHART:
+        case CONSTANTS.HORIZONTAL_LINE_CHART:
+        case CONSTANTS.HORIZONTAL_AREA_CHART:
+
+        case CONSTANTS.RADAR_CHART:
             if (!xLabelName) {
                 xLabelName = dataLabels[0];
             }
@@ -46,6 +54,20 @@ const getToolTopValues = (element) => {
             tooltipHtmlValue.push(formTooltipRow(xLabelName, dataValues.valueX1));
             tooltipHtmlValue.push(formTooltipRow(colorByName, dataValues.valueX2));
             tooltipHtmlValue.push(formTooltipRow(yLabelName, dataValues.valueY1));
+            break;
+        case CONSTANTS.HEAT_MAP:
+            if (!xLabelName) {
+                xLabelName = dataLabels[0];
+            }
+            if (!yLabelName) {
+                yLabelName = dataLabels[1];
+            }
+            if (!colorByName) {
+                colorByName = dataLabels[2];
+            }
+            tooltipHtmlValue.push(formTooltipRow(xLabelName, dataValues.valueX1));
+            tooltipHtmlValue.push(formTooltipRow(yLabelName, dataValues.valueY1));
+            tooltipHtmlValue.push(formTooltipRow(colorByName, dataValues.value));
             break;
         case CONSTANTS.SCATTER_CHART:
             if (!xLabelName) {
@@ -76,6 +98,28 @@ const getToolTopValues = (element) => {
             tooltipHtmlValue.push(formTooltipRow(xLabelName, dataValues.valueX1));
             tooltipHtmlValue.push(formTooltipRow(yLabelName, dataValues.valueY1));
             break;
+        case CONSTANTS.SUNBURST_CHART:
+            xLabelName = dataValues.valueXLabel;
+            yLabelName = dataValues.valueYLabel;
+            if (!(xLabelName == "undefined" && dataValues.valueX1 == yLabelName)) {
+                tooltipHtmlValue.push(formTooltipRow(xLabelName, dataValues.valueX1));
+            }
+            tooltipHtmlValue.push(formTooltipRow(yLabelName, dataValues.valueY1));
+            break;
+        case CONSTANTS.GROUP_BAR_CHART:
+        case CONSTANTS.HORIZONTAL_GROUP_BAR_CHART:
+            if (!xLabelName) {
+                xLabelName = dataLabels[0];
+            }
+            if (!yLabelName) {
+                yLabelName = dataLabels[2];
+            }
+            if (!colorByName) {
+                colorByName = dataLabels[1];
+            }
+            tooltipHtmlValue.push(formTooltipRow(xLabelName, dataValues.valueX1));
+            tooltipHtmlValue.push(formTooltipRow(colorByName, dataValues.valueX2));
+            tooltipHtmlValue.push(formTooltipRow(yLabelName, dataValues.valueY));
     }
 
     return tooltipHtmlValue.join("");
@@ -101,11 +145,16 @@ const setTooltipHandler = () => {
             // Get the x and y values for the mouse position
             const pointers = d3.pointer(event, this);
             let [xpos, ypos] = pointers;
+            let topValue = 0,
+                leftValue = 0;
+
+            const { chartName } = grafieks.plotConfiguration;
 
             if (
-                grafieks.plotConfiguration.chartName == CONSTANTS.HORIZONTAL_STACKED_BAR_CHART ||
-                grafieks.plotConfiguration.chartName == CONSTANTS.STACKED_BAR_CHART ||
-                grafieks.plotConfiguration.chartName == CONSTANTS.WATERFALL_CHART
+                chartName == CONSTANTS.HORIZONTAL_STACKED_BAR_CHART ||
+                chartName == CONSTANTS.STACKED_BAR_CHART ||
+                chartName == CONSTANTS.WATERFALL_CHART ||
+                chartName == CONSTANTS.GROUP_BAR_CHART
             ) {
                 const parentElement = this.parentElement;
                 const matrix = window.getComputedStyle(parentElement).transform;
@@ -114,8 +163,31 @@ const setTooltipHandler = () => {
                 xpos += +matrixValues[4] || 0;
             }
 
-            const topValue = ypos - 30 + (window.grafieks.legend.topMargin || 0);
-            const leftValue = xpos + 20 + (window.grafieks.legend.leftMargin || 0);
+            if (grafieks.plotConfiguration.isAxisBasedChart) {
+                topValue = ypos - 30 + (window.grafieks.legend.topMargin || 0);
+                leftValue = xpos + 20 + (window.grafieks.legend.leftMargin || 0);
+            }
+
+            if (
+                chartName == CONSTANTS.RADAR_CHART ||
+                chartName == CONSTANTS.PIE_CHART ||
+                chartName == CONSTANTS.DONUT_CHART ||
+                chartName == CONSTANTS.SUNBURST_CHART ||
+                chartName == CONSTANTS.HORIZONTAL_GROUP_BAR_CHART
+            ) {
+                topValue = event.clientY - 30;
+                leftValue = event.clientX + 20;
+                xpos = event.clientX;
+                ypos = event.clientY + 20;
+            }
+
+            if (topValue < 0) {
+                topValue = 0;
+            }
+
+            if (leftValue < 0) {
+                leftValue = 0;
+            }
 
             // Set the tooltip position
             d3.select(".tooltip")
@@ -135,9 +207,12 @@ const setTooltipHandler = () => {
 
             const tooltipBox = d3.select(".tooltip").node();
             tooltipBox.style.right = null;
+            tooltipBox.style.bottom = null;
 
             // Check if tooltip is in viewport
-            if (!isElementInViewport(d3.select(".tooltip").node())) {
+            const tooltipNode = d3.select(".tooltip").node();
+            const { status: inViewStatus, boundingRects } = isElementInViewport(tooltipNode);
+            if (!inViewStatus && boundingRects.right > (window.innerWidth || document.documentElement.clientWidth)) {
                 d3.select(".tooltip .leftArrow").style("display", "none");
                 d3.select(".tooltip .rightArrow").style("display", "block");
 
@@ -145,6 +220,10 @@ const setTooltipHandler = () => {
 
                 tooltipBox.style.right = toolTipRight + 16 + "px";
                 tooltipBox.style.left = null;
+            }
+            if (!inViewStatus && boundingRects.bottom > (window.innerHeight || document.documentElement.clientHeight)) {
+                tooltipBox.style.bottom = "0px";
+                tooltipBox.style.top = null;
             }
 
             // Fade all the other lines
